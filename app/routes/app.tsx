d5a730e577943571fb37f868229d9a39995139e8
amgst <amgst99@gmail.com>
2026-04-29 12:33:30 +0500

    g

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,17 +1,20 @@
-import { Outlet } from "@remix-run/react";
-import { AppProvider } from "@shopify/polaris";
-import "@shopify/polaris/build/esm/styles.css";
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
+
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await authenticate.admin(request);
-  return null;
+  return json({ apiKey: process.env.SHOPIFY_API_KEY ?? "" });
 }
 
 export default function AppLayout() {
+  const { apiKey } = useLoaderData<typeof loader>();
   return (
-    <AppProvider i18n={{}}>
+    <AppProvider isEmbeddedApp apiKey={apiKey}>
       <Outlet />
     </AppProvider>
   );
